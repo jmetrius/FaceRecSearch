74122dd0c56e956c61895515bfd3364d9714dda4
Jan Sonntag <jaso35@googlemail.com>
2022-01-26 19:28:38 +0100

    Initial commit 2

---
+++ FaceRecSearch.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+
+import argparse
+import face_recognition as fr
+import numpy as np
+import os
+import shutil
+import sys
+import time
+import concurrent.futures
+
+
+def list_full_paths(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory)]
+
+
+class FaceRecSearch:
+
+    image_file_types = ('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG')
+    image_files = []
+
+    def __init__(self, _options):
+        self.source_directory = os.path.abspath(_options.sourcedir)
+        if not os.path.isdir(self.source_directory):
+            print('ERROR: source directory must exist')
+            sys.exit(1)
+
+        if not any(filename.endswith(self.image_file_types)
+           for filename in os.listdir(self.source_directory)):
+            print('ERROR: source directory must contain image files')
+            sys.exit(1)
+
+        if _options.targetdir:
+            self.target_directory = os.path.abspath(_options.outputdir)
+        else:
+            parser.print_help()
+            print('ERROR: target directory must be specified')
+            sys.exit(1)
+        if not os.path.isdir(self.target_directory):
+            print('ERROR: target directory must exist')
+            sys.exit(1)
+
+        if _options.facedir:
+            self.faces_directory = os.path.abspath(_options.facedir)
+        else:
+            parser.print_help()
+            print('ERROR: faces directory must be specified')
+            sys.exit(1)
+        if not os.path.isdir(self.faces_directory):
+            print('ERROR: faces directory must exist')
+            sys.exit(1)
+
+        if not any(filename.endswith(self.image_file_types)
+           for filename in os.listdir(self.faces_directory)):
+            print('ERROR: faces directory must contain image files')
+            sys.exit(1)
+
+        self.cpus = _options.cpus
+        self.recursive = _options.recursive
+        self.tolerance = _options.tolerance
+
+        self.processed_face_encodings = []
+
+    def process_image(self, file):
+        print(f'Started processing {file}...')
+        try:
+            image = fr.load_image_file(file)
+            face_locations = fr.face_locations(
+                image, number_of_times_to_upsample=0)
+            face_encodings = fr.face_encodings(image, face_locations)
+            print(f'Processed {file}...')
+            return file, face_encodings
+
+        except Exception as err:
+            print('ERROR: %s' % err)
+
+    def run(self):
+        if self.recursive:
+            for root, directories, files in os.walk(self.source_directory):
+                for file in files:
+                    if file.endswith(self.image_file_types):
+                        self.image_files.append(os.path.join(root, file))
+        else:
+            for file in os.listdir(self.source_directory):
+                if file.endswith(self.image_file_types):
+                    self.image_files.append(
+                        os.path.join(self.source_directory, file))
+
+        for face in list_full_paths(self.faces_directory):
+            print(f'Started scanning for target faces in {face}...')
+            try:
+                image = fr.load_image_file(face)
+                face_locations = fr.face_locations(image)
+                if len(face_locations) > 1:
+                    print('Multiple faces found, only using the first one')
+                face_encoding = fr.face_encodings(
+                    image, face_locations)
+                print(f'Processed {face}...')
+                self.processed_face_encodings.append(face_encoding[0])
+            except Exception as err:
+                print('ERROR: %s' % err)
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.cpus) as executor:
+            print('Running jobs...')
+            results = executor.map(self.process_image, self.image_files)
+            counter = 0
+            for file, face_encodings in results:
+                print(f'Checking {file}...')
+                try:
+                    for face_encoding in face_encodings:
+                        matches = fr.compare_faces(self.processed_face_encodings, face_encoding, self.tolerance)
+
+                        if np.any(matches):
+                            print(f'Face found in {file}!')
+                            counter = counter + 1
+                            shutil.copyfile(file, os.path.join(
+                                self.target_directory, str(counter) + '_' + os.path.basename(file)))
+                            break
+                except Exception as err:
+                    print('ERROR: %s' % err)
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    desc = " FaceRecSearch - Quick and dirty image search using facial recognition"
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('sourcedir', help='where to search for matching images')
+    parser.add_argument('outputdir', help='where found images should be stored')
+    parser.add_argument('facedir', help='directory containing examples of the face you are looking for')
+    parser.add_argument('--tolerance', default=0.6, help='distance between faces to consider it a match, lower is more strict.', metavar='0.6')
+    parser.add_argument('--cpus', default=os.cpu_count(), help="number of CPUs to use, defaults to all")
+    parser.add_argument('--recursive', action='store_false', help='search in all subdirectories of sourcedir')
+    options = parser.parse_args()
+
+    if not (options.sourcedir and options.outputdir and options.facedir):
+        parser.print_help()
+        print('ERROR: source, face and output directory must be specified')
+        sys.exit(1)
+
+    face_search = FaceRecSearch(options)
+    face_search.run()
+
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 2)} second(s)')
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # FaceRecSearch
-Search your pictures for specific people using facial recognition
+This script attempts to search for people in pictures by comparing all faces in the picture with found faces in a prespecified set of images. All found pictures will be copied and stored in the output folder.
+
+### Installing
+This script depends on [face_recognition](https://github.com/ageitgey/face_recognition) for all the heavy lifting.
+Install it first by running:
+```bash
+$ pip install face_recognition
+```
+Afterwards, clone this Git repository or download the script separatly:
+```bash
+$ git clone https://github.com/jmetrius/FaceRecSearch.git
+$ cd FaceRecSearch
+```
+
+## Usage
+```bash
+$ python FaceRecSearch.py [-h] [--tolerance 0.6] [--cpus CPUS] [--recursive] sourcedir outputdir facedir
+
+FaceRecSearch - Quick and dirty image search using facial recognition
+
+positional arguments:
+  sourcedir        where to search for matching images
+  outputdir        where found images should be stored
+  facedir          directory containing examples of the face you are looking for
+
+options:
+  -h, --help       show this help message and exit
+  --tolerance 0.6  distance between faces to consider it a match, lower is more strict.
+  --cpus CPUS      number of CPUs to use, defaults to all
+  --recursive      search in all subdirectories of sourcedir
+```
+
+* The default tolerance is based on the recommendations of the face_recognition module. For my use case, a stricter setting of 0.45 was better but your experience might differ.
+* If a picture in facedir contains faces of multiple people, only the first one will be used. This might lead to unexpected behaviour, therefore make sure only one face is visible in each example picture.
+* It's advisable to place multiple example pictures of the same person (showing the face in different angles or with different expressions) in facedir. You can also place pictures of different people if you want to search for more than one person at once. However, since every example face is compared with all faces of pictures in the sourcedir, runtime may increase drastically with additional faces. 2-3 pictures per person should be enough.
+
+## Credits
+Credit goes to greatwhitehat for providing [faceoff](https://github.com/greatwhitehat/faceoff) which served as the inital codebase for this script.
